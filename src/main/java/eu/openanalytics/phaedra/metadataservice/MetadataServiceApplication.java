ebe64701c01b14ae182668e1b164f87efdc27fc2
sberberovic <sasa.berberovic@openanalytics.eu>
2021-09-03 15:15:34 +0200

    Remove unused imports

--- src/main/java/eu/openanalytics/phaedra/metadataservice/MetadataServiceApplication.java
+++ src/main/java/eu/openanalytics/phaedra/metadataservice/MetadataServiceApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
