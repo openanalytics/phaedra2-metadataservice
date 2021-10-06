f629e365e9b395e0598c6d2298699d78308df93c
sberberovic <sasa.berberovic@openanalytics.eu>
2021-10-06 16:50:48 +0200

    Add EXPERIMENT to ObjectClass enum

--- src/main/java/eu/openanalytics/phaedra/metadataservice/enumeration/ObjectClass.java
+++ src/main/java/eu/openanalytics/phaedra/metadataservice/enumeration/ObjectClass.java
@@ -3,7 +3,8 @@ package eu.openanalytics.phaedra.metadataservice.enumeration;
 public enum ObjectClass {
     WELL,
     PLATE,
+    EXPERIMENT,
     FEATURE,
     PROTOCOL,
-    PROJECT
+    PROJECT,
 }
