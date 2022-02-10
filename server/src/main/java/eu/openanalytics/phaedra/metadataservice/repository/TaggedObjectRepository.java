6091cb9c34589d37d5dbc30d845eea24c801cacd
Frederick Michielssen <fmichielssen@openanalytics.eu>
2022-02-10 11:21:43 +0100

    Fixed delete query for TaggedObjects

--- server/src/main/java/eu/openanalytics/phaedra/metadataservice/repository/TaggedObjectRepository.java
+++ server/src/main/java/eu/openanalytics/phaedra/metadataservice/repository/TaggedObjectRepository.java
@@ -3,6 +3,7 @@ package eu.openanalytics.phaedra.metadataservice.repository;
 import java.util.List;
 import java.util.Set;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -12,6 +13,7 @@ import eu.openanalytics.phaedra.metadataservice.model.TaggedObject;
 @Repository
 public interface TaggedObjectRepository extends CrudRepository<TaggedObject, Long> {
 	
+	@Modifying
 	@Query("delete from hca_tagged_object hto where hto.tag_id = :tagId and hto.object_id = :objectId and hto.object_class = :objectClass")
 	void deleteByTagIdAndObjectIdAndObjectClass(Long tagId, Long objectId, String objectClass);
 	
